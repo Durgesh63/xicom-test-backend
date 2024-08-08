7d73170af9ea149d0ed464a31bd6af43883dec14
durgesh63 <durgeshmaurya0998@gmail.com>
2024-08-09 02:34:43 +0530

    unused file delete

--- src/Routes/candidate.routes.js
+++ src/Routes/candidate.routes.js
@@ -7,6 +7,10 @@ const candidaterouter = Router()
 // candidaterouter.route("/register").post(Upload.fields([{
 //     name: "documents"
 // }]), register)
+candidaterouter.route("/is-live").get((req, res) => {
+    res.send("active")
+})
+
 candidaterouter.route("/register").post(Upload.any(), register)
 
 
